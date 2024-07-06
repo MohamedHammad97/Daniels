55c1e5c1ec659e7d222a327686ab6d77cf40eb47
MohamedHammad97 <mohamed.sayed.hammad97@gmail.com>
2024-07-06 15:22:48 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var typing = new Typed('#typed', {
+    strings: ["Larry Daniles", "Developer", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+});
+
+let { top: aboutSection } = $("#about").offset()
+$(window).scroll(function () {
+    let windowOffset = $(window).scrollTop()
+    if (windowOffset >aboutSection -50) {
+        $("#navBar").css("background-color","rgba(0, 0, 0, 0.8)")
+        $("#navBar .container").css("padding", "0 100px")
+        $("#backToTop").fadeIn(1000)
+    } else {
+        $("#navBar").css("background-color", "transparent")
+        $("#navBar .container").css("padding", "0 12px")
+        $("#backToTop").fadeOut(1000)
+    }
+})
+
+$("#backToTop").click(function () {
+    $("html,body").animate({scrollTop:0},100)
+})
+
+$(document).ready(function () {
+    $(".loader").fadeOut(1000,function () {
+        $("#loading").fadeOut(1000,function () {
+            $("body").css("overflow","auto")
+        })
+    })
+})
+
+
+$('.count').counterUp()
